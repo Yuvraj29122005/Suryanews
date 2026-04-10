e2db4208cd016c918e042f93495c39478ada6579
YUVRAJ DHADHAL <yuvrajdhadhal988@gmail.com>
2026-04-10 11:18:04 +0530

    category wise added

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
         let {title, description, imageUrl, url} = this.props;
         return (
             <div className="my-3">
-                <div className="card" style={{width: "18rem"}}>
+                <div className="card" >
                     <img src={!imageUrl?"https://assets3.cbsnewsstatic.com/hub/i/r/2026/04/06/17e36424-6d35-49f3-bab3-fc039eb10935/thumbnail/1200x630/72bf7bdae245f400a09786e9431ce726/gettyimages-2194273729.jpg":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
